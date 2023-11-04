0472745a4f8e66579bed49939d1f716e9e153f70
Parikshit Kumar (─‿‿─) <abha.bharti35@gmail.com>
2023-11-04 18:51:03 +0530

    Delete client directory

--- client/src/components/pay/Razorpay.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Razorpay = () => {
-  return <div>Razorpay</div>;
-};
-
-export default Razorpay;
--- client/src/pages/ChatSupport.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Chat from "../components/chat/Chat";
-
-const ChatSupport = () => {
-  return <Chat />;
-};
-
-export default ChatSupport;
